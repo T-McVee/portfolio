d1a6fc5ee3fa6b0470eb96626cad12952d7d3936
Tim McVinish <tim@Tims-MacBook-Pro.local>
2021-11-13 22:44:18 -0800

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Splash } from './components/Splash';
+import { Bio } from './components/Bio';
+import { Work } from './components/Work';
+import { Footer } from './components/Footer';
+
 function App() {
-  return <div className="App">Poop</div>;
+  return (
+    <div className="App">
+      <Splash />
+      <Bio />
+      <Work />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
