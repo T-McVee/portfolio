90d9d0924ce917caa9c9817d1a21430b2da473cd
Tim McVinish <tim@Tims-MacBook-Pro.local>
2021-11-14 11:03:35 -0800

    Updates Splash component styles

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,60 +1,56 @@
 import styled from 'styled-components';
+import bgImage from '../img/splash-bg.jpg';
+import iconGh from '../img/github-brands.svg';
+import iconLi from '../img/linkedin-in-brands.svg';
+import iconEmail from '../img/envelope-regular.svg';
 
 export const Splash = () => {
   return (
     <Section>
-      <Container>
-        <LogoWrapper>
-          <Logo>
-            T-<span className="bold">McVee</span>
-          </Logo>
-        </LogoWrapper>
-        <header>
-          <HeaderUpper>
-            <H1>
-              Tim <br /> <span className="bold">McVinish</span>
-            </H1>
-            <Button>get in contact</Button>
-          </HeaderUpper>
-          <HeaderLower>
-            <HeaderLowerText>
-              <H2>
-                Web Dev{' '}
-                <span className="bold">
-                  / <br />
-                  Marketer
-                </span>
-              </H2>
-              <P>
-                Hi, my name’s Tim McVinish. I love using technology to create
-                amazing experiences and solve problems.
-              </P>
-            </HeaderLowerText>
-            <Icons>
-              <Icon>Git</Icon>
-              <Icon>Li</Icon>
-              <Icon>eMail</Icon>
-            </Icons>
-          </HeaderLower>
-        </header>
-      </Container>
+      <LogoWrapper>
+        <Logo>
+          T-<span className="bold">McVee</span>
+        </Logo>
+      </LogoWrapper>
+      <Header>
+        <HeaderUpper>
+          <H1>
+            Tim <br /> <span className="bold">McVinish</span>
+          </H1>
+          <Button>get in contact</Button>
+        </HeaderUpper>
+        <HeaderLower>
+          <HeaderLowerText>
+            <H2>
+              Web Dev{' '}
+              <span className="bold">
+                / <br />
+                Marketer
+              </span>
+            </H2>
+            <P>
+              Hi, my name’s Tim McVinish. I love using technology to create
+              engaging experiences and solve problems.
+            </P>
+          </HeaderLowerText>
+          <Icons>
+            <Icon src={iconGh} />
+            <Icon src={iconLi} />
+            <Icon src={iconEmail} />
+          </Icons>
+        </HeaderLower>
+      </Header>
     </Section>
   );
 };
 
 const Section = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   width: 100%;
   height: 100vh;
   padding: 5.5rem;
-`;
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  // border: 1px solid ${(props) => props.theme.colorAccent1};
+  background-image: url(${bgImage});
+  background-position: center;
+  background-size: cover;
 `;
 
 const LogoWrapper = styled.div`
@@ -66,9 +62,14 @@ const Logo = styled.div`
   font-size: 1.5rem;
 `;
 
+const Header = styled.header`
+  width: 100%;
+  height: calc(100% - 80px);
+`;
+
 const HeaderUpper = styled.div`
   width: 100%;
-  height: calc(200px - 1rem);
+  height: calc(100% - 14rem);
   border-right: 1px solid ${(props) => props.theme.colorBlack};
 `;
 
@@ -77,6 +78,7 @@ const H1 = styled.h1`
   font-weight: 400;
   text-transform: uppercase;
   line-height: 4rem;
+  margin-bottom: 1rem;
 `;
 
 const Button = styled.div`
@@ -84,14 +86,22 @@ const Button = styled.div`
   padding: 0.5rem 1rem;
   border: 1px solid ${(props) => props.theme.colorBlack};
   border-radius: ${(props) => props.theme.radiusSmall};
+  transition: background-color 0.3s;
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${(props) => props.theme.colorAccent1};
+    border-color: ${(props) => props.theme.colorAccent1};
+    color: ${(props) => props.theme.colorWhite};
+  }
 `;
 
 const HeaderLower = styled.div`
   display: flex;
   justify-content: right;
   width: 100%;
-  height: 100%;
-  padding: 1.5rem 0;
+  height: 224px;
+  padding: 1.5rem 0 0;
 `;
 
 const HeaderLowerText = styled.div`
@@ -107,7 +117,9 @@ const H2 = styled.h2`
   font-weight: 400;
   text-transform: uppercase;
   text-align: right;
+  line-height: 2.75rem;
   width: fit-content;
+  margin-bottom: 0.75rem;
 `;
 
 const P = styled.p`
@@ -122,10 +134,14 @@ const Icons = styled.div`
   width: 3rem;
 `;
 
-const Icon = styled.i`
+const Icon = styled.img`
   position: relative;
   transform: translate(50%);
   padding: 0.75rem;
+
+  &:hover {
+    cursor: pointer;
+  }
 
   &:nth-of-type(1) {
     padding-top: 0;
