88e8325aa284f347b16d9d083aa9df8f36c30920
T-McVee <iam@tmcvee.com>
2021-11-24 08:07:32 -0800

    Adds the lower cta and removes unused code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Splash } from './components/Splash';
 import { Bio } from './components/Bio';
 import { Work } from './components/Work';
 import { Skills } from './components/Skills';
+import { LowerCta } from './components/LowerCta';
 import { Footer } from './components/Footer';
 import {
   faHtml5,
@@ -16,9 +17,6 @@ import logoBigCommerce from './img/logos/logoBigCommerce.png';
 import logoXd from './img/logos/logoXd.png';
 import logoPhotoshop from './img/logos/logoPhotoshop.png';
 import { ThemeProvider } from 'styled-components';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Slider from 'react-slick';
 
 function App() {
   return (
@@ -28,6 +26,7 @@ function App() {
         <Bio />
         <Work />
         <Skills skills={skills} />
+        <LowerCta />
         <Footer />
       </ThemeProvider>
     </div>
